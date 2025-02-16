680aa938ca7593206265e5bd8df92d0a4290f24f
Vladislav <vladskorikov@gmail.com>
2025-02-16 18:54:47 +0400

    prod config

--- app/config.py
+++ app/config.py
@@ -8,6 +8,7 @@ PG_HOST = os.getenv("POSTGRES_HOST")
 PG_PORT = os.getenv("POSTGRES_PORT")
 PG_DB = os.getenv("POSTGRES_DB")
 DATABASE_URL = f'postgresql+asyncpg://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{PG_DB}'
+PLATFORM_URL = os.getenv("PLATFORM_URL", 'http://127.0.0.1')
 
 #JWT TOKEN CONFIG
 SECRET_KEY = "your_secret_key"
@@ -16,7 +17,6 @@ ACCESS_TOKEN_EXPIRE_DAYS = 1
 
 STATIC_PATH = '../static'
 CHUNK_SIZE = 5 * 1024 * 1024  # 5 MB
-PLATFORM_URL = 'http://127.0.0.1'
 
 
 class PrizeQualityEnum(Enum):
